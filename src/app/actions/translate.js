bec54edc3a2ee4238661d22b78df0991564c76a0
prabeshgupta <prabeshtech232@gmail.com>
2025-03-01 22:52:52 +0545

    Lanuage translation feature

---
+++ src/app/actions/translate.js
@@ -0,0 +1,28 @@
+'use server'
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+async function translateText(text, targetLanguage, languageFrom = "") {
+    const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = languageFrom ? `Translate from ${languageFrom} to ${targetLanguage}: ${text}` : `Detect the language of the text and translate it to ${targetLanguage}: ${text}`;
+
+    const additional_prompt = "Just return the translated text. Do not add additional descriptions such as `Here are the translations:` or `The translation is:`";
+
+    try {
+        const result = await model.generateContent(prompt + " " + additional_prompt);
+        return result.response.text();
+    } catch (error) {
+        console.log(error);
+    }
+    return "Could not translate the text";
+}
+
+export async function translate(formData) {
+    const text = formData.get('text');
+    const targetLanguage = formData.get('targetLanguage');
+    const languageFrom = formData.get('languageFrom');
+
+    const translation = await translateText(text, targetLanguage, languageFrom);
+    return { translation };
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import { Dropdown } from "@/app/components/dropdown";
 import { useState } from "react";
+import { translate } from "@/app/actions/translate";
 
 const languageOptions = [
   {
@@ -25,6 +26,9 @@ export default function Home() {
   const [languageFrom, setLanguageFrom] = useState("en");
   const [languageTo, setLanguageTo] = useState("fr");
 
+  const [inputText, setInputText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+
   const handleLanguageFromChange = (value) => {
     setLanguageFrom(value);
   }
@@ -33,29 +37,50 @@ export default function Home() {
     setLanguageTo(value);
   }
 
+  const handleInputChange = (e) => {
+    const newText = e.target.value;
+    setInputText(newText);
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-row gap-4">
-          <div className="container flex flex-col">
-            <Dropdown
-              name='LanguageFrom'
-              options={languageOptions}
-              value={languageFrom}
-              onChange={handleLanguageFromChange} />
-            <textarea className="border border-slate-800 rounded-md p-4 " />
+        <form action={async (formData) => {
+          const result = await translate(formData);
+          setTranslatedText(result.translation);
+        }}>
+          <div className="flex flex-row gap-4">
+            <div className="container flex flex-col">
+              <Dropdown
+                name='languageFrom'
+                options={languageOptions}
+                value={languageFrom}
+                onChange={handleLanguageFromChange} />
+              <textarea
+                name="text"
+                placeholder="Enter the text to translate"
+                className="border border-slate-800 rounded-md p-4 lg:w-[400px]"
+                value={inputText}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="container flex flex-col">
+              <Dropdown
+                name='targetLanguage'
+                options={languageOptions}
+                value={languageTo}
+                onChange={handleLanguageToChange} />
+              <textarea
+                placeholder="Translated text shows up here"
+                className="border border-slate-800 rounded-md p-4 lg:w-[400px]"
+                value={translatedText}
+                readOnly
+              />
+            </div>
           </div>
-          <div className="container flex flex-col">
-            <Dropdown
-              name='LanguageFrom'
-              options={languageOptions}
-              value={languageTo}
-              onChange={handleLanguageToChange} />
-            <textarea className="border border-slate-800 rounded-md p-4 " />
-          </div>
-        </div>
-        <button className="p-3 rounded-md bg-slate-800 text-white">Translate</button>
+          <button type='submit' className="p-3 rounded-md bg-slate-800 text-white mt-4">Translate</button>
+        </form>
       </main>
-    </div>
+    </div >
   );
 }
